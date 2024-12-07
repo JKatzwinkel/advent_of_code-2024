6ea654829acefda291f655c6f72a18c93c2aa513
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2024-12-07 01:28:30 +0100

    [06b] implement placement of new obstacles

--- 241206/j.py
+++ 241206/j.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+from copy import copy
+from enum import Enum
 from io import TextIOBase
+from typing import Self
 
 
 DIRS = [(0, -1), (1, 0), (0, 1), (-1, 0)]
@@ -21,7 +24,7 @@ def load_line(y: int, line: str, game: Game) -> None:
     tiles = []
     for x, char in enumerate(line):
         if char == '^':
-            game.guard = Guard(game.board, (x, y))
+            game.guard = Guard(game, (x, y))
         tiles.append(char == '#')
     game.board.tiles.extend(tiles)
 
@@ -34,10 +37,46 @@ def test_load() -> None:
     assert game.guard.pos == (4, 6)
 
 
+class State(Enum):
+    LEFT = 1
+    STUCK = 2
+
+
 class Game:
     def __init__(self):
         self.guard = None
         self.board = None
+
+    def loop(self) -> State:
+        while not self.guard.outside() and not self.guard.stuck():
+            self.guard.move()
+        if self.guard.outside():
+            return State.LEFT
+        return State.STUCK
+
+    def __copy__(self) -> Self:
+        result = self.__class__()
+        result.guard = copy(self.guard)
+        result.guard.game = result
+        result.board = copy(self.board)
+        return result
+
+    def with_obstacle(self, pos: tuple[int, int]) -> Self | None:
+        if pos == self.guard.pos:
+            return None
+        result = copy(self)
+        result.board = self.board.with_obstacle(pos)
+        if not result.board:
+            return None
+        return result
+
+
+def test_copy_game_with_obstacle() -> None:
+    with open('test.txt') as f:
+        game = load(f)
+    copied = game.with_obstacle((3, 6))
+    assert copied.board[3, 6] == '#'
+    assert game.board[3, 6] == '.'
 
 
 class Board:
@@ -58,6 +97,24 @@ class Board:
         x, y = pos
         return '#' if self.tiles[x + y * self.width] else '.'
 
+    def __setitem__(self, pos: tuple[int, int], value: str) -> None:
+        x, y = pos
+        self.tiles[x + y * self.width] = value in ('#', 'O')
+
+    def __copy__(self) -> Self:
+        result = self.__class__()
+        result.width = self.width
+        result.height = self.height
+        result.tiles = copy(self.tiles)
+        return result
+
+    def with_obstacle(self, pos: tuple[int, int]) -> Self | None:
+        if self[pos] == '#':
+            return None
+        result = copy(self)
+        result[pos] = 'O'
+        return result
+
 
 def test_sample_board() -> None:
     with open('test.txt') as f:
@@ -68,10 +125,10 @@ def test_sample_board() -> None:
 
 
 class Guard:
-    def __init__(self, board: Board, pos: tuple[int, int]):
+    def __init__(self, game: Game, pos: tuple[int, int]):
         self.pos = pos
         self.dir = 0
-        self.board = board
+        self.game = game
         self.visited = {self.pos}
 
     def tile_ahead(self) -> tuple[int, int]:
@@ -80,18 +137,27 @@ class Guard:
         return x, y
 
     def move(self) -> None:
-        while self.board[self.tile_ahead()] == '#':
+        while self.game.board[self.tile_ahead()] == '#':
             self.dir += 1
         self.pos = self.tile_ahead()
-        if self.pos in self.board:
+        if self.pos in self.game.board:
             self.visited.add(self.pos)
 
     def outside(self) -> bool:
-        return self.pos not in self.board
+        return self.pos not in self.game.board
+
+    def stuck(self) -> bool:
+        return False
 
     @property
     def steps(self) -> int:
         return len(self.visited)
+
+    def __copy__(self) -> Self:
+        result = self.__class__(self.game, self.pos)
+        result.dir = self.dir
+        result.visited = copy(self.visited)
+        return result
 
 
 def test_move_guard() -> None:
@@ -104,8 +170,7 @@ def test_move_guard() -> None:
 def test_test_input() -> None:
     with open('test.txt') as f:
         game = load(f)
-    while not game.guard.outside():
-        game.guard.move()
+    assert game.loop() == State.LEFT
     assert game.guard.steps == 41
 
 
