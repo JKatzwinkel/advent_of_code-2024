5a252d9a23d2c2d3c7c5e65f8f0219e65d9c0d56
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2024-12-11 14:25:42 +0100

    [11a] fuck it graph tiem

--- 241211/j.py
+++ 241211/j.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from math import floor, log10
+from typing import Self
 
 import pytest
 
@@ -41,6 +42,99 @@ def change(stone: int) -> list[int]:
 
 def test_change_rules() -> None:
     assert change(99) == [9, 9]
+    assert change(2024) == [20, 24]
+    assert change(1) == [2024]
+    # assert change(0, 2) == [2024]
+    # assert change(0, 3) == [20, 24]
+
+
+class Node:
+    def __init__(self, stone: int) -> None:
+        self.value = stone
+        self.children: list[Node] = []
+        self.populated = False
+
+    def add(self, child: Node) -> Self:
+        self.children.append(child)
+        return self
+
+    def traverse(self, length: int) -> list[Node]:
+        if length == 0:
+            return [self]
+        return [
+            node
+            for child in self.children
+            for node in child.traverse(length - 1)
+        ]
+
+
+class Graph:
+    def __init__(self) -> None:
+        self.nodes: dict[int, Node] = {}
+
+    def __getitem__(self, stone: int) -> Node:
+        result = self.nodes.get(stone, Node(stone))
+        if stone not in self.nodes:
+            self.nodes[stone] = result
+        return result
+
+    def __len__(self) -> int:
+        return len(self.nodes)
+
+    def link(self, stone: int, changes_into: int) -> Self:
+        self[stone].add(self[changes_into])
+        return self
+
+    def traverse(self, start: int, length: int) -> list[int]:
+        return [
+            node.value for node in self[start].traverse(length)
+        ]
+
+    def keys(self) -> set[int]:
+        return set(self.nodes.keys())
+
+    def __contains__(self, stone: int) -> bool:
+        return stone in self.nodes
+
+    @classmethod
+    def spawn(cls) -> Graph:
+        g = Graph()
+        frontier: set[int] = {0}
+        while frontier:
+            new_frontier = set()
+            for stone in frontier:
+                for successor in change(stone):
+                    g.link(stone, successor)
+                    if g[successor].populated:
+                        continue
+                    new_frontier.add(successor)
+                g[stone].populated = True
+            frontier = new_frontier
+        return g
+
+
+@pytest.fixture
+def smol_graph() -> Graph:
+    g = Graph().link(0, 1).link(1, 2024).link(2024, 20).link(2024, 24)
+    return g.link(20, 2).link(20, 0).link(24, 2).link(24, 4)
+
+
+def test_graph(smol_graph: Graph) -> None:
+    assert len(smol_graph) == 7
+    assert smol_graph[20].value == 20
+
+
+def test_traverse_graph(smol_graph: Graph) -> None:
+    assert smol_graph.traverse(1, 2) == [20, 24]
+    assert smol_graph.traverse(1, 3) == [2, 0, 2, 4]
+
+
+def test_spawn_graph() -> None:
+    g = Graph.spawn()
+    assert g[20].children == [g[2], g[0]]
+    assert g.traverse(0, 6) == [
+        40, 48, 2024, 40, 48, 80, 96
+    ]
 
 
 def blink(stones: list[int], times: int = 1) -> list[int]:
@@ -97,6 +191,5 @@ def test_input() -> None:
 
 if __name__ == '__main__':
     stones = read('4022724 951333 0 21633 5857 97 702 6')
-    for _ in range(25):
-        stones = blink(stones)
-    print(len(stones))
+    print(len(blink(stones, 25)))
+    # print(len(blink(stones, 75)))
