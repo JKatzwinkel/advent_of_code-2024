37cf684747a4e5f760bfab85a5bb25e2ce05f11e
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2024-12-12 13:58:19 +0100

    [12a] fix region merge in plot loading

--- 241212/j.py
+++ 241212/j.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from collections import defaultdict
 from io import StringIO, TextIOBase
-from typing import Self
+from typing import Iterable, Iterator, Self
 
 import pytest
 
@@ -33,8 +33,7 @@ def medion_example() -> StringIO:
 @pytest.fixture
 def large_example() -> StringIO:
     return StringIO(
-        '''
-        RRRRIICCFF
+        '''RRRRIICCFF
         RRRRIICCCF
         VVRRRCCFFF
         VVRCCCJFFF
@@ -47,42 +46,69 @@ def large_example() -> StringIO:
     )
 
 
+class Loader:
+    def __init__(self, src: TextIOBase) -> None:
+        self.plot_regions: dict[Point, Region] = defaultdict(Region)
+        self.src = src
+
+    def merge(self, pos1: Point, pos2: Point) -> Region:
+        merged = self.plot_regions[pos1].add_all(
+            self.plot_regions[pos2]
+        )
+        for plot in merged:
+            self.plot_regions[plot] = merged
+        return merged
+
+    def load(self) -> list[Region]:
+        z = 0
+        prev = ''
+        for row in self.src:
+            if not (plots := row.split('\n')[0].strip()):
+                continue
+            for x, plant in enumerate(plots):
+                self.plot_regions[x, z].add((x, z))
+                if z > 0 and prev[x] == plant:
+                    self.merge((x, z-1), (x, z))
+                if x > 0 and plots[x-1] == plant:
+                    self.merge((x-1, z), (x, z))
+                else:
+                    self.plot_regions[x, z].plant = plant
+            z += 1
+            prev = plots
+        return sorted(list(set(self.plot_regions.values())))
+
+
 def load(src: TextIOBase) -> list[Region]:
-    regions: dict[Point, Region] = defaultdict(Region)
-    z = 0
-    prev = ''
-    for row in src:
-        if not (plots := row.split('\n')[0].strip()):
-            continue
-        for x, plant in enumerate(plots):
-            if z > 0 and prev[x] == plant:
-                regions[x, z] = regions[x, z-1]
-            elif x > 0 and plots[x-1] == plant:
-                regions[x, z] = regions[x-1, z]
-            else:
-                regions[x, z].plant = plant
-            regions[x, z].plots.add((x, z))
-        z += 1
-        prev = plots
-    return sorted(list(set(regions.values())))
+    return Loader(src).load()
 
 
-def test_load(smol_example: StringIO) -> None:
+def test_smol(smol_example: StringIO) -> None:
     regions = load(smol_example)
     assert len(regions) == 5
     assert regions[0].area == 4
     assert regions[0].plant == 'A'
+    assert [region.price for region in regions] == [
+        40, 32, 40, 4, 24
+    ]
 
 
-def test_load_medion(medion_example: StringIO) -> None:
+def test_medion(medion_example: StringIO) -> None:
     regions = load(medion_example)
     assert len(regions) == 5
     assert regions[0].plant == 'O'
+    assert [region.price for region in regions] == [
+        756, 4, 4, 4, 4
+    ]
 
 
-def test_load_large(large_example: StringIO) -> None:
+def test_large(large_example: StringIO) -> None:
     regions = load(large_example)
     assert len(regions) == 11
+    assert regions[0].plant == 'C'
+    assert regions[0].price == 4
+    assert regions[1].plant == 'C'
+    assert regions[1].price == 392
+    assert sum(region.price for region in regions) == 1930
 
 
 type Point = tuple[int, int]
@@ -94,10 +120,9 @@ def step_into(v: Point, pos: Point) -> Point:
     return x, z
 
 
-class Region:
+class Region(Iterable[Point]):
     '''
-    >>> r = Region()
-    >>> r.plant = 'A'
+    >>> r = Region('A')
     >>> r.add((1, 1))
     <Region[A]{(1, 1)}>
     >>> r.area
@@ -107,9 +132,9 @@ class Region:
     >>> r.add((2, 1)).perimeter
     6
     '''
-    def __init__(self) -> None:
+    def __init__(self, plant: str = '') -> None:
         self.plots: set[Point] = set()
-        self.plant = ''
+        self.plant = plant
 
     @property
     def area(self) -> int:
@@ -136,6 +161,17 @@ class Region:
             return self.area < other.area
         return self.plant < other.plant
 
+    def __iter__(self) -> Iterator[Point]:
+        self._iter = iter(self.plots)
+        return self
+
+    def __next__(self) -> Point:
+        return next(self._iter)
+
+    def add_all(self, other: Self) -> Self:
+        self.plots.update(other.plots)
+        return self
+
     def add(self, plot: Point) -> Self:
         self.plots.add(plot)
         return self
@@ -145,29 +181,15 @@ class Region:
         return self.area * self.perimeter
 
 
+def test_region_comparator() -> None:
+    a = Region('A').add((1, 1)).add((2, 1))
+    b = Region('A').add((3, 1))
+    assert max(a, b) == a
+    assert sorted([a, b])[-1] == a
+
+
 def test_perimeter(medion_example: StringIO) -> None:
     regions = load(medion_example)
     assert regions[0].plant == 'O'
     assert regions[0].perimeter == 36
     assert regions[1].perimeter == 4
-
-
-def test_price_01(smol_example: StringIO) -> None:
-    regions = load(smol_example)
-    assert regions[0].plant == 'A'
-    assert [region.price for region in regions] == [40, 32, 40, 4, 24]
-
-
-def test_price_02(medion_example: StringIO) -> None:
-    regions = load(medion_example)
-    assert regions[0].plant == 'O'
-    assert [region.price for region in regions] == [756, 4, 4, 4, 4]
-
-
-def test_price_03(large_example: StringIO) -> None:
-    regions = load(large_example)
-    assert regions[0].plant == 'C'
-    assert regions[0].price == 4
-    assert regions[1].plant == 'C'
-    assert regions[1].price == 392
-    assert sum(region.price for region in regions) == 1930
