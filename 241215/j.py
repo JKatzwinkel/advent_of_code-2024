90b378ccefb57795410e908515b08ce4158b10ae
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2024-12-16 03:10:57 +0100

    [15b] implement double-wide grid && solve part 2

--- 241215/j.py
+++ 241215/j.py
@@ -13,7 +13,7 @@ def load(src: TextIOBase) -> Grid:
     for line in src:
         if not (row := line.strip()):
             continue
-        if all(c in '.#@O' for c in row):
+        if all(c in '.#@O[]' for c in row):
             grid += [c for c in row]
         else:
             moves.extend([c for c in row])
@@ -27,29 +27,76 @@ class Grid:
         self.height = 0
         self.cells: list[str] = []
         self.robot = Robot(self, -1, -1)
+        self._scale = 1
 
-    def boxes_pushed(
+    def scale(self) -> Self:
+        self._scale = 2
+        cells = []
+        for y in range(self.height):
+            row = []
+            for c in self.cells[
+                y*self.width:(y+1)*self.width
+            ]:
+                if c == 'O':
+                    row.extend(['[', ']'])
+                else:
+                    row.extend([c] * 2)
+            cells.extend(row)
+        self.cells = cells
+        self.width *= 2
+        self.robot.pos = (
+            self.robot.pos[0] * 2,
+            self.robot.pos[1]
+        )
+        return self
+
+    def pushable_boxes(
         self, pos: tuple[int, int], direction: int
-    ) -> int:
-        boxes = 0
-        while self[(pos := step(pos, direction))] == 'O':
-            boxes += 1
-        if self[pos] == '#':
-            return -1
+    ) -> set[tuple[tuple[int, int], bool]]:
+        boxes = set()
+        adj = step(pos, direction)
+        if self[adj] == '#':
+            return {(pos, False)}
+        boxes.add((pos, True))
+        if self[adj] == '.':
+            return boxes
+        if self[adj] in 'O[]':
+            boxes.update(
+                self.pushable_boxes(adj, direction)
+            )
+        if direction in [0, 2]:
+            if self[adj] == '[':
+                boxes.update(
+                    self.pushable_boxes(
+                        step(adj, 1), direction
+                    )
+                )
+            elif self[adj] == ']':
+                boxes.update(
+                    self.pushable_boxes(
+                        step(adj, 3), direction
+                    )
+                )
         return boxes
 
-    def push_boxes(
-        self, pos: tuple[int, int], direction: int,
-        boxes: int
-    ) -> Self:
-        self[step(pos, direction)] = '.'
-        self[step(pos, direction, boxes + 1)] = 'O'
-        return self
+    def push(
+        self, pos: tuple[int, int], direction: int
+    ) -> bool:
+        pushable = self.pushable_boxes(pos, direction)
+        if not all(b for _, b in pushable):
+            return False
+        changes = []
+        for p, _ in pushable:
+            changes.append((step(p, direction), self[p]))
+            self[p] = '.'
+        for p, c in changes:
+            self[p] = c
+        return True
 
     def gps(self) -> int:
         result = 0
         for i, c in enumerate(self.cells):
-            if c != 'O':
+            if c not in 'O[':
                 continue
             y, x = divmod(i, self.width)
             result += y * 100 + x
@@ -74,6 +121,8 @@ class Grid:
                 cells[:cells.index('@')] + ['.']
                 + cells[cells.index('@')+1:]
             )
+        if '[' in cells:
+            self._scale = 2
         self.cells += cells
         self.width = len(cells)
         self.height += 1
@@ -81,8 +130,8 @@ class Grid:
 
     def __str__(self) -> str:
         lines = [
-            self.cells[i*self.width:(i+1)*self.width]
-            for i in range(self.height)
+            self.cells[y*self.width:(y+1)*self.width]
+            for y in range(self.height)
         ]
         lines[self.robot.pos[1]][self.robot.pos[0]] = '@'
         return '\n'.join(
@@ -115,47 +164,40 @@ class Robot:
         direction = self.__class__.MOVES.index(
             move or self.moves.pop(0)
         )
-        if (
-            effect := self.grid.boxes_pushed(
-                self.pos, direction
-            )
-        ) < 0:
+        if not self.grid.push(self.pos, direction):
             return self
-        if effect:
-            self.grid.push_boxes(
-                self.pos, direction, effect
-            )
         self.pos = step(self.pos, direction)
         return self
 
 
+LARGE = '''
+   ##########
+   #..O..O.O#
+   #......O.#
+   #.OO..O.O#
+   #..O@..O.#
+   #O#..O...#
+   #O..O..O.#
+   #.OO.O.OO#
+   #....O...#
+   ##########
+
+   <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+   vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+   ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+   <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+   ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+   ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+   >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+   <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+   ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+   v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+   '''
+
+
 @pytest.fixture
 def large() -> StringIO:
-    return StringIO(
-        '''
-        ##########
-        #..O..O.O#
-        #......O.#
-        #.OO..O.O#
-        #..O@..O.#
-        #O#..O...#
-        #O..O..O.#
-        #.OO.O.OO#
-        #....O...#
-        ##########
-
-        <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
-        vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
-        ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
-        <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
-        ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
-        ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
-        >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
-        <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
-        ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
-        v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
-        '''
-    )
+    return StringIO(LARGE)
 
 
 @pytest.fixture
@@ -185,18 +227,16 @@ def test_load(small: StringIO) -> None:
 
 @pytest.mark.parametrize(
     'x, pushed',
-    [(0, 0), (1, 1), (3, 2), (6, -1)]
+    [(0, True), (1, True), (3, True), (6, False)]
 )
-def test_boxes_pushed(x: int, pushed: int) -> None:
+def test_boxes_pushed(x: int, pushed: bool) -> None:
     grid = load(StringIO('..O.OO.OOO#'))
-    assert grid.boxes_pushed((x, 0), 1) == pushed
+    assert grid.push((x, 0), 1) == pushed
 
 
 def test_push_boxes() -> None:
     grid = load(StringIO('.@OOO.'))
-    grid.push_boxes(
-        (1, 0), 1, grid.boxes_pushed((1, 0), 1)
-    )
+    grid.push((1, 0), 1)
     assert str(grid) == '.@.OOO'
 
 
@@ -263,3 +303,145 @@ def test_input_part1() -> None:
     while grid.robot.moves:
         grid.robot.move()
     assert grid.gps() == 1475249
+
+
+def test_scale_large(large: StringIO) -> None:
+    grid = load(large).scale()
+    assert grid.width == 20
+    assert str(grid) == dedent(
+        '''\
+        ####################
+        ##....[]....[]..[]##
+        ##............[]..##
+        ##..[][]....[]..[]##
+        ##....[]@.....[]..##
+        ##[]##....[]......##
+        ##[]....[]....[]..##
+        ##..[][]..[]..[][]##
+        ##........[]......##
+        ####################'''
+    )
+
+
+def test_push_double_boxes_large(large: StringIO) -> None:
+    grid = load(large).scale()
+    while grid.robot.moves:
+        grid.robot.move()
+    expected = dedent(
+        '''\
+        ####################
+        ##[].......[].[][]##
+        ##[]...........[].##
+        ##[]........[][][]##
+        ##[]......[]....[]##
+        ##..##......[]....##
+        ##..[]............##
+        ##..@......[].[][]##
+        ##......[][]..[]..##
+        ####################'''
+    )
+    assert str(grid) == expected
+    assert grid.gps() == 9021
+
+
+def test_push_double_boxes() -> None:
+    grid = load(StringIO(
+        '''
+        ##############
+        ##......##..##
+        ##..........##
+        ##...[][]...##
+        ##....[]....##
+        ##.....@....##
+        ##############'''
+    ))
+    grid.robot.move('^')
+    grid.robot.move('^')
+    assert str(grid) == dedent(
+        '''\
+        ##############
+        ##......##..##
+        ##...[][]...##
+        ##....[]....##
+        ##.....@....##
+        ##..........##
+        ##############'''
+    )
+
+
+def test_push_double_boxes_2() -> None:
+    grid = load(StringIO(
+        '''
+        ##############
+        ##......##..##
+        ##..........##
+        ##....[][]@.##
+        ##....[]....##
+        ##..........##
+        ##############
+        <vv<<^^<<^^'''
+    ))
+    while grid.robot.moves:
+        grid.robot.move()
+    assert str(grid) == dedent(
+        '''\
+        ##############
+        ##...[].##..##
+        ##...@.[]...##
+        ##....[]....##
+        ##..........##
+        ##..........##
+        ##############'''
+    )
+
+
+def test_push_double_boxes_3() -> None:
+    grid = load(StringIO(
+        '''
+        ####################
+        ##....[]....[]..[]##
+        ##............[]..##
+        ##..[][]....[]..[]##
+        ##...[].......[]..##
+        ##[]##....[]......##
+        ##[]......[]..[]..##
+        ##..[][]..@[].[][]##
+        ##........[]......##
+        ####################'''
+    ))
+    grid.robot.move('^')
+    assert str(grid) == dedent(
+        '''\
+        ####################
+        ##....[]....[]..[]##
+        ##............[]..##
+        ##..[][]....[]..[]##
+        ##...[]...[]..[]..##
+        ##[]##....[]......##
+        ##[]......@...[]..##
+        ##..[][]...[].[][]##
+        ##........[]......##
+        ####################'''
+    )
+
+
+def test_input_part2() -> None:
+    with open('input.txt') as f:
+        grid = load(f).scale()
+    while grid.robot.moves:
+        grid.robot.move()
+    assert grid.gps() == 1509724
+
+
+if __name__ == '__main__':
+    import time
+    grid = load(StringIO(LARGE)).scale()
+    print(f'{grid}')
+    while grid.robot.moves:
+        grid.robot.move()
+        for i in range(grid.height):
+            print(
+                f'\033[A{" " * grid.width}\033[A'
+            )
+        print(f'{grid}')
+        time.sleep(.02)
