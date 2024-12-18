6921427ffa9b29503ffbea29ab5f73ca9e61ccbf
jkatzwinkel <JKatzwinkel@users.noreply.github.com>
2024-12-18 14:19:44 +0100

    [18a] add pathfinding && solve part 1

--- 241218/j.py
+++ 241218/j.py
@@ -42,11 +42,11 @@ def load(src: TextIOBase, width: int) -> Mem:
 
 class Mem:
     def __init__(
-        self, width: int, falling_bytes: list[tuple[int, int]]
+        self, width: int, falling_bytes: list[P]
     ) -> None:
         self.width = width
         self.falling = falling_bytes
-        self.corrupted: set[tuple[int, int]] = set()
+        self.corrupted: set[P] = set()
 
     def fall(self, times: int = 1) -> Self:
         while times > 0 and self.falling:
@@ -54,8 +54,15 @@ class Mem:
             times -= 1
         return self
 
-    def __getitem__(self, pos: tuple[int, int]) -> bool:
+    def escape(self) -> list[P]:
+        return Pathfinder(self).find().result
+
+    def __getitem__(self, pos: P) -> bool:
         return pos in self.corrupted
+
+    def __contains__(self, pos: P) -> bool:
+        x, y = pos
+        return 0 <= x < self.width and 0 <= y < self.width
 
     def __str__(self) -> str:
         lines = [
@@ -66,6 +73,77 @@ class Mem:
         return '\n'.join(
             ''.join(line) for line in lines
         )
+
+
+type P = tuple[int, int]
+DIRS = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+
+
+def step(pos: P, direction: tuple[int, int]) -> P:
+    '''
+    >>> step((1, 1), (0, -1))
+    (1, 0)
+    '''
+    x, y = map(sum, zip(pos, direction))
+    return x, y
+
+
+def dist(a: P, b: P) -> int:
+    '''
+    >>> dist((1, 2), (5, 0))
+    6
+    '''
+    dx, dy = map(lambda t: t[1]-t[0], zip(a, b))
+    return abs(dx) + abs(dy)
+
+
+class Pathfinder:
+    def __init__(self, mem: Mem) -> None:
+        self.m = mem
+        self.visited: dict[P, tuple[P, int]] = {}
+        self.frontier = [((0, 0), (0, 0), 0)]
+        self.target = (mem.width - 1, mem.width - 1)
+
+    def find(self, target: P | None = None) -> Self:
+        self.target = target or self.target
+        while self.search():
+            ...
+        return self
+
+    def search(self) -> bool:
+        node = min(self.frontier, key=self._score)
+        pos, via, cost = self.frontier.pop(self.frontier.index(node))
+        if not self._improves(pos, cost):
+            return len(self.frontier) > 0
+        self.visited[pos] = (via, cost)
+        if pos == self.target:
+            self.backtrack(pos)
+            return False
+        for direction in DIRS:
+            adj = step(pos, direction)
+            if self._improves(adj, cost + 1):
+                self.frontier.append(
+                    (adj, pos, cost + 1)
+                )
+        return len(self.frontier) > 0
+
+    def backtrack(self, pos: P) -> None:
+        path = []
+        while pos != (0, 0):
+            path.append(pos)
+            pos, _ = self.visited[pos]
+        self.result = path[::-1]
+
+    def _score(self, node: tuple[P, P, int]) -> int:
+        pos, via, cost = node
+        return cost + dist(pos, self.target)
+
+    def _improves(self, pos: P, cost: int) -> bool:
+        if pos not in self.m or self.m[pos]:
+            return False
+        if pos not in self.visited:
+            return True
+        return self.visited[pos][1] > cost
 
 
 def test_load() -> None:
@@ -89,3 +167,32 @@ def test_plot() -> None:
         .#..#..
         #.#....'''
     ), f'{mem}'
+
+
+def test_pathfinder() -> None:
+    mem = load(StringIO('1, 0\n1, 1'), 3).fall(2)
+    pf = Pathfinder(mem).find((2, 2))
+    assert len(pf.result) == 4
+
+
+def test_pathfinder_example() -> None:
+    mem = load(StringIO(EXAMPLE), 7)
+    mem.fall(12)
+    p = mem.escape()
+    assert len(p) == 22, f'{p}'
+
+
+def test_input_part1() -> None:
+    with open('input.txt') as f:
+        mem = load(f, 71)
+    mem.fall(1024)
+    p = mem.escape()
+    assert len(p) == 282
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        mem = load(f, 71)
+    mem.fall(1024)
+    p = mem.escape()
+    print(len(p))
