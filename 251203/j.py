d0b3ab64a856fc833263d2b7bb6aa10991d4195a
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2025-12-03 19:42:57 +0100

    [3b] support 12 joltage digits

--- 251203/j.py
+++ 251203/j.py
@@ -15,17 +15,38 @@ def test_joltage_example(pack: int, jolt: int) -> None:
     assert joltage(pack) == jolt
 
 
-def joltage(pack: int | str) -> int:
-    a, i = maxat(f'{pack}'[:-1])
-    b, _ = maxat(f'{pack}'[i+1:])
-    return a * 10 + b
+@pytest.mark.parametrize(
+    'pack,jolt', (
+        (987654321111111, 987654321111),
+        (811111111111119, 811111111119),
+        (234234234234278, 434234234278),
+        (818181911112111, 888911112111),
+    )
+)
+def test_joltage_example2(pack: int, jolt: int) -> None:
+    assert joltage(pack, 12) == jolt
+
+
+def joltage(pack: int | str, figs: int = 2) -> int:
+    digits = []
+    i = 0
+    while figs > 0:
+        figs -= 1
+        n, j = maxat(
+            f'{pack}'[i:-figs] if figs else f'{pack}'[i:]
+        )
+        digits.append(n)
+        i += j
+    return sum(n * 10 ** i for i, n in enumerate(reversed(digits)))
 
 
 def maxat(s: str) -> tuple[int, int]:
-    m, j = 0, -1
+    m, j = 0, 0
     for i, c in enumerate(s):
         if (n := int(c)) > m:
-            m, j = n, i
+            m, j = n, i + 1
+        if n == 9:
+            break
     return m, j
 
 
