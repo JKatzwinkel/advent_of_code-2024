e27378ebfb41f911f63ee3844f488db21ff802d1
Jakob Höper <jakob.hoeper@sbb.spk-berlin.de>
2025-12-05 13:56:04 +0100

    [5a] solve part 1

--- 251205/j.py
+++ 251205/j.py
@@ -1,3 +1,5 @@
+import pathlib
+
 import pytest
 
 
@@ -46,3 +48,17 @@ def isfresh(ingredient: int, ranges: list[tuple[int, int]]) -> bool:
 def test_freshnesz(ingredient: int, expected: bool) -> None:
     ranges, _ = load(X)
     assert isfresh(ingredient, ranges) is expected
+
+
+def count_fresh(ingredients: list[int], ranges: list[tuple[int, int]]) -> int:
+    '''
+    >>> rr, ii = load(X)
+    >>> count_fresh(ii, rr)
+    3
+    '''
+    return sum(isfresh(i, ranges) for i in ingredients)
+
+
+if __name__ == '__main__':
+    rr, ii = load(pathlib.Path('input.txt').read_text())
+    print(count_fresh(ii, rr))
