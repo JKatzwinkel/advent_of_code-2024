00d554af520e5248c037962abab2fe95ca34b598
Jakob Höper <jakob.hoeper@sbb.spk-berlin.de>
2025-12-05 18:10:48 +0100

    [5b] solve part 2

--- 251205/j.py
+++ 251205/j.py
@@ -59,6 +59,59 @@ def count_fresh(ingredients: list[int], ranges: list[tuple[int, int]]) -> int:
     return sum(isfresh(i, ranges) for i in ingredients)
 
 
+def overlap(r1: tuple[int, int], r2: tuple[int, int]) -> bool:
+    '''
+    >>> overlap((1, 3), (5, 6))
+    False
+    >>> overlap((1, 3), (0, 4))
+    True
+    >>> overlap((1, 3), (4, 5))
+    True
+    >>> overlap((1, 4), (2, 3))
+    True
+    >>> overlap((2, 6), (5, 8))
+    True
+    '''
+    (a, b), (c, d) = sorted([r1, r2])
+    return c < b + 2
+
+
+def fold(ranges: list[tuple[int, int]]) -> list[tuple[int, int]]:
+    '''
+    >>> fold([(2, 4), (3, 5), (4, 6)])
+    [(2, 6)]
+    '''
+    result = [ranges[0]]
+    for a, b in ranges[1:]:
+        if overlap((a, b), (r := result[-1])):
+            result[-1] = ((min(a, r[0]), max(b, r[1])))
+        else:
+            result.append((a, b))
+    return result
+
+
+def merge(ranges: list[tuple[int, int]]) -> list[tuple[int, int]]:
+    '''
+    >>> merge([(5, 8), (2, 6)])
+    [(2, 8)]
+
+    >>> rr, _ = load(X)
+    >>> merge(rr)
+    [(3, 5), (10, 20)]
+    '''
+    return fold(sorted(ranges))
+
+
+def size(ranges: list[tuple[int, int]]) -> int:
+    '''
+    >>> rr, _ = load(X)
+    >>> size(rr)
+    14
+    '''
+    return sum(1 + b - a for a, b in merge(ranges))
+
+
 if __name__ == '__main__':
     rr, ii = load(pathlib.Path('input.txt').read_text())
     print(count_fresh(ii, rr))
+    print(size(rr))
