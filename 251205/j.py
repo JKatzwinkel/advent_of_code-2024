6e9aca4d942501f231a0f688c9dcef8d580ca0a5
Jakob Höper <jakob.hoeper@sbb.spk-berlin.de>
2025-12-05 13:49:50 +0100

    [5a] determine freshnesz

--- 251205/j.py
+++ 251205/j.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 X = '''\
 3-5
 10-14
@@ -29,3 +32,17 @@ def load(src: str) -> tuple[list[tuple[int, int]], list[int]]:
         a, b = list(map(int, line.split('-')))
         ranges.append((a, b))
     return ranges, iids
+
+
+def isfresh(ingredient: int, ranges: list[tuple[int, int]]) -> bool:
+    return any(a <= ingredient <= b for a, b in ranges)
+
+
+@pytest.mark.parametrize(
+    'ingredient,expected', (
+        (1, False), (5, True), (8, False), (11, True), (17, True), (32, False),
+    )
+)
+def test_freshnesz(ingredient: int, expected: bool) -> None:
+    ranges, _ = load(X)
+    assert isfresh(ingredient, ranges) is expected
