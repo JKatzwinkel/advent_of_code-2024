76d2b5a09d8cee32d3e52944ba7c589ee273ed5e
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2025-12-08 18:09:13 +0100

    [8a] sort example points by pairwise distance

---
+++ 251208/j.py
@@ -0,0 +1,74 @@
+import itertools
+
+
+X = '''
+162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689
+'''
+
+
+type Point = tuple[int, int, int]
+
+
+def load(src: str) -> list[Point]:
+    '''
+    >>> load(X)[0]
+    (162, 817, 812)
+
+    >>> len(load(X))
+    20
+    '''
+    return [
+        tuple(map(int, line.split(',')))  # type: ignore
+        for line in src.split()
+    ]
+
+
+def distance(a: Point, b: Point) -> float:
+    '''
+    >>> distance((162,817,812), (425,690,689))
+    100427
+    '''
+    return sum((v1 - v2) ** 2 for v1, v2 in zip(a, b))
+
+
+def closest_pairs(points: list[Point]) -> list[tuple[Point, Point]]:
+    '''
+    >>> cp = closest_pairs(load(X))
+    >>> cp[0]
+    ((162, 817, 812), (425, 690, 689))
+    >>> cp[1]
+    ((162, 817, 812), (431, 825, 988))
+    >>> cp[2]
+    ((906, 360, 560), (805, 96, 715))
+    >>> cp[3]
+    ((431, 825, 988), (425, 690, 689))
+    '''
+    return sorted(
+        itertools.combinations(points, 2),
+        key=lambda pair: distance(pair[0], pair[1]),
+    )
+
+
+if __name__ == '__main__':
+    cp = closest_pairs(load(X))
+    for i, (a, b) in enumerate(cp):
+        print(f'{i}: {a} {b} - {distance(a, b)}')
