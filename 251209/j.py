2c5c732c949244ea4db0d13f73f7db7cef953da8
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2025-12-09 21:47:47 +0100

    [9b] compute directions that fence corners face

--- 251209/j.py
+++ 251209/j.py
@@ -12,6 +12,18 @@ X = '''
 2,3
 7,3'''
 
+F = '''
+..............
+.......#XXX#..
+.......X...X..
+..#XXXX#...X..
+..X........X..
+..#XXXXXX#.X..
+.........X.X..
+.........#X#..
+..............
+'''
+
 
 type Point = tuple[int, int]
 
@@ -56,6 +68,54 @@ def bigrect(points: list[Point]) -> tuple[int, Point, Point]:
     pairs = sorted(combinations(points, 2), key=lambda p: area(*p))
     a, b = pairs[-1]
     return area(a, b), a, b
+
+
+def sig(n: int) -> int:
+    '''
+    >>> sig(-4)
+    -1
+    >>> sig(0)
+    0
+    >>> sig(1)
+    1
+    '''
+    if n == 0:
+        return 0
+    return n // abs(n)
+
+
+def left(a: Point, b: Point) -> Point:
+    '''
+    >>> left((7, 1), (11, 1))
+    (0, -1)
+
+    >>> left((7, 3), (7, 1))
+    (-1, 0)
+
+    >>> left((11, 1), (11, 7))
+    (1, 0)
+
+    >>> left((9, 5), (2, 5))
+    (0, 1)
+    '''
+    return sig(b[1] - a[1]), sig(a[0] - b[0])
+
+
+def out(corner: tuple[Point, Point, Point]) -> Point:
+    '''
+    >>> pp = load(X)
+    >>> out(pp[:3])
+    (1, -1)
+    >>> out(pp[1:4])
+    (1, 1)
+    >>> out(pp[2:5])
+    (-1, 1)
+    '''
+    return tuple(  # type: ignore
+        w + v for w, v in zip(
+            left(*corner[:2]), left(*corner[1:3])
+        )
+    )
 
 
 if __name__ == '__main__':
