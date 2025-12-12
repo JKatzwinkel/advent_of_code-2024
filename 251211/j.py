8d3297176f6e59be5acc46a6561d2c3d82d22e9d
Jakob Höper <jakob.hoeper@sbb.spk-berlin.de>
2025-12-12 23:08:19 +0100

    [11b] reproduce example from part 2

--- 251211/j.py
+++ 251211/j.py
@@ -37,6 +37,7 @@ def load(src: str) -> dict[str, list[str]]:
 
 def find(
     edges: dict[str, list[str]],
+    *checkpoints: str,
     start: str = 'you', end: str = 'out',
 ) -> list[list[str]]:
     '''
@@ -54,7 +55,10 @@ def find(
             for adj in edges[node]
             for tail in recurse(adj)
         ]
-    return recurse(start)
+    return [
+        tail for tail in recurse(start)
+        if all(checkpoint in tail for checkpoint in checkpoints)
+    ]
 
 
 def test_pathfinding() -> None:
@@ -67,6 +71,49 @@ def test_pathfinding() -> None:
         ['you', 'ccc', 'eee', 'out'],
         ['you', 'ccc', 'fff', 'out'],
     ]
+
+
+Y = '''
+svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+'''
+
+
+E = '''\
+svr,aaa,fft,ccc,ddd,hub,fff,ggg,out
+svr,aaa,fft,ccc,ddd,hub,fff,hhh,out
+svr,aaa,fft,ccc,eee,dac,fff,ggg,out
+svr,aaa,fft,ccc,eee,dac,fff,hhh,out
+svr,bbb,tty,ccc,ddd,hub,fff,ggg,out
+svr,bbb,tty,ccc,ddd,hub,fff,hhh,out
+svr,bbb,tty,ccc,eee,dac,fff,ggg,out
+svr,bbb,tty,ccc,eee,dac,fff,hhh,out'''
+
+
+def test_pathfinding_custom_start() -> None:
+    gg = load(Y)
+    pp = find(gg, start='svr')
+    assert len(pp) == 8
+    assert '\n'.join(','.join(path) for path in pp) == E
+
+
+def test_pathfinding_with_checkpoints() -> None:
+    gg = load(Y)
+    pp = find(gg, 'fft', 'dac', start='svr')
+    assert len(pp) == 2
+    assert ','.join(pp[0]) == 'svr,aaa,fft,ccc,eee,dac,fff,ggg,out'
+    assert ','.join(pp[1]) == 'svr,aaa,fft,ccc,eee,dac,fff,hhh,out'
 
 
 if __name__ == '__main__':
